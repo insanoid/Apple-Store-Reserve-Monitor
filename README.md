ab7eaee0c036f3b152571d8bda36e2183f434d11
Karthikeya Udupa K M <karthikeyaudupa@gmail.com>
2016-09-20 13:05:37 +0200

    improved code quality, added option to select a device, only shows selected store.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Apple Store Reserve Monitor
 ==============================
 
-A simple script to monitor status of the iPhone availability in a given country. Can be changed to any country and any version of the required device. Makes an audio announcement if there is a change in the stock for the selected/preferred store.
+A simple script to monitor status of the iPhone availability in a given country. Can be changed to any country and any version of the required device. Makes an audio announcement if there is a change in the stock for the selected store for the selected device.
 
 ![image](listing.png)
--- storecheck.py
+++ storecheck.py
@@ -16,17 +16,10 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-import json
-import urllib
-import urllib2
-import httplib
-import sys
-import datetime
-from dateutil import parser
-from bs4 import BeautifulSoup
 import requests
 import os
 import time
+import sys
 
 ##
 reload(sys)
@@ -40,87 +33,79 @@ class bcolors:
     FAIL = '\033[91m'
     ENDC = '\0330m'
 
-# Change this to alter the monitoring stores.
-pref_stores = ['R358', 'R396', 'R352']
+# Change this to alter the monitoring stores and devices
+pref_stores = ['R358', 'R396', 'R352', 'R659']
+pref_models = ['MN4M2', 'MN972']
 
 
 # Change the country and language for your location for example (DE/de_DE, GB/en_GB)
-storeurl = "https://reserve.cdn-apple.com/DE/de_DE/reserve/iPhone/stores.json"
-availurl = "https://reserve.cdn-apple.com/DE/de_DE/reserve/iPhone/availability.json"
+storeurl = "https://reserve.cdn-apple.com/GB/en_GB/reserve/iPhone/stores.json"
+availurl = "https://reserve.cdn-apple.com/GB/en_GB/reserve/iPhone/availability.json"
 
 
-def decode(k):
-    # Since the ending of the device code changes based on region, we have to ignore it.
-    if len(k) >= 5:
-        k = k[0:5]
-    # Incase of a new version, update models here.
-    model_mapping  = {
-        "MN962": "iPhone 7 Jet Black - 128 GB",
-        "MN9C2": "iPhone 7 Jet Black - 256 GB",
-        "MN8X2": "iPhone 7 Black - 32 GB",
-        "MN922": "iPhone 7 Black - 128 GB",
-        "MN972": "iPhone 7 Black - 256 GB",
-        "MN902": "iPhone 7 Gold - 32 GB",
-        "MN942": "iPhone 7 Gold - 128 GB",
-        "MN992": "iPhone 7 Gold - 256 GB",
-        "MN912": "iPhone 7 Rose Gold - 32 GB",
-        "MN952": "iPhone 7 Rose Gold - 128 GB",
-        "MN9A2": "iPhone 7 Rose Gold - 256 GB",
-        "MN8Y2": "iPhone 7 Silver - 32 GB",
-        "MN932": "iPhone 7 Silver - 128 GB",
-        "MN982": "iPhone 7 Silver - 256 GB",
-        "MN4V2": "iPhone 7 Plus Jet Black - 128 GB",
-        "MN512": "iPhone 7 Plus Jet Black - 256 GB",
-        "MNQM2": "iPhone 7 Plus Black - 32 GB",
-        "MN4M2": "iPhone 7 Plus Black - 128 GB",
-        "MN4W2": "iPhone 7 Plus Black - 256 GB",
-        "MNQQ2": "iPhone 7 Plus Rose Gold - 32 GB",
-        "MN4U2": "iPhone 7 Plus Rose Gold- 128 GB",
-        "MN4Y2": "iPhone 7 Plus Rose Gold - 256 GB",
-        "MNQP2": "iPhone 7 Plus Gold - 32 GB",
-        "MN4Q2": "iPhone 7 Plus Gold - 128 GB",
-        "MN502": "iPhone 7 Plus Gold - 256 GB",
-        "MNQN2": "iPhone 7 Plus Silver - 32 GB",
-        "MN4P2": "iPhone 7 Plus Silver - 128 GB",
-        "MN4X2": "iPhone 7 Plus Silver - 256 GB",
-		"timeSlot":""
-    }
-
-    model_name = model_mapping.get(k)
-    if(model_name == None or len(model_name) <= 0):
-        model_name = None
-
-    return model_name
-
-
-
+# Incase of a new version, update models here.
+model_mapping  = {
+    "MN962": "iPhone 7 Jet Black - 128 GB",
+    "MN9C2": "iPhone 7 Jet Black - 256 GB",
+    "MN8X2": "iPhone 7 Black - 32 GB",
+    "MN922": "iPhone 7 Black - 128 GB",
+    "MN972": "iPhone 7 Black - 256 GB",
+    "MN902": "iPhone 7 Gold - 32 GB",
+    "MN942": "iPhone 7 Gold - 128 GB",
+    "MN992": "iPhone 7 Gold - 256 GB",
+    "MN912": "iPhone 7 Rose Gold - 32 GB",
+    "MN952": "iPhone 7 Rose Gold - 128 GB",
+    "MN9A2": "iPhone 7 Rose Gold - 256 GB",
+    "MN8Y2": "iPhone 7 Silver - 32 GB",
+    "MN932": "iPhone 7 Silver - 128 GB",
+    "MN982": "iPhone 7 Silver - 256 GB",
+    "MN4V2": "iPhone 7 Plus Jet Black - 128 GB",
+    "MN512": "iPhone 7 Plus Jet Black - 256 GB",
+    "MNQM2": "iPhone 7 Plus Black - 32 GB",
+    "MN4M2": "iPhone 7 Plus Black - 128 GB",
+    "MN4W2": "iPhone 7 Plus Black - 256 GB",
+    "MNQQ2": "iPhone 7 Plus Rose Gold - 32 GB",
+    "MN4U2": "iPhone 7 Plus Rose Gold- 128 GB",
+    "MN4Y2": "iPhone 7 Plus Rose Gold - 256 GB",
+    "MNQP2": "iPhone 7 Plus Gold - 32 GB",
+    "MN4Q2": "iPhone 7 Plus Gold - 128 GB",
+    "MN502": "iPhone 7 Plus Gold - 256 GB",
+    "MNQN2": "iPhone 7 Plus Silver - 32 GB",
+    "MN4P2": "iPhone 7 Plus Silver - 128 GB",
+    "MN4X2": "iPhone 7 Plus Silver - 256 GB",
+	"timeSlot":""
+}
 
 store_json = requests.get(storeurl).json()
 avail_json = requests.get(availurl).json()
-items_available = False
+
 avail_in_required = False
 
 if 'stores' in store_json:
-    for key in store_json['stores']:
-        items =  avail_json.get(key.get('storeNumber'))
-        print bcolors.OKGREEN + str(key.get('storeName')) + ", " + str(key.get('storeCity'))
+    for store in store_json['stores']:
+        items =  avail_json.get(store.get('storeNumber'))
+        store_in_prefer = store.get('storeNumber') in pref_stores
+        if store_in_prefer:
+            print('{}{}, {} ({})'.format(bcolors.OKGREEN, store.get('storeName'), store.get('storeCity'), store.get('storeNumber')))
         items_available = False
-        for k in items:
-            if items.get(k) != "NONE":
-                value = decode(k)
-                if value != None:
-                    print bcolors.WARNING + "    -    " + value
+        for model_code in items:
+            if items.get(model_code) != "NONE":
+                partial_model_code = model_code[0:5]
+                model_name = model_mapping.get(partial_model_code, None)
+                if model_name != None:
                     items_available = True;
-                if key.get('storeNumber') in pref_stores:
-                    avail_in_required = True;
+                    if store_in_prefer and partial_model_code in pref_models:
+                        avail_in_required = True;
+                        print('{}    -    {}'.format(bcolors.FAIL, model_name))
+                    elif store_in_prefer:
+                        print('{}    -    {}'.format(bcolors.OKBLUE, model_name))
 
-        if items_available == False:
-            print bcolors.FAIL + "Nothing Available\n"
+        if not items_available and store_in_prefer:
+            print('{}Nothing Available\n'.format(bcolors.FAIL))
+    print('{}Updated: {}\n'.format(bcolors.OKBLUE, time.strftime('%d, %b %Y %H:%M:%S')))
 
-    print bcolors.OKBLUE + "Updated: "+ time.strftime('%d, %b %Y %H:%M:%S')  + "\n"
-
-    if avail_in_required == True:
+    if avail_in_required:
         os.system('say "iPhone is Available in the required store."')
 
 else:
-    print bcolors.FAIL + time.strftime('%d, %b %Y %H:%M:%S') + " - Data Unavailable."
+    print('{}Data Unavailable: {}\n'.format(bcolors.OKBLUE, time.strftime('%d, %b %Y %H:%M:%S')))
